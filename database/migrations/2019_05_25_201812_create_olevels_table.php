5707f43e9310bb8d8c386d97cbf8f264d1624af9
abas essien <abasifreke100@gmail.com>
2019-05-25 21:28:20 +0100

    models,Controllers,migrations created

---
+++ database/migrations/2019_05_25_201812_create_olevels_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOlevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('olevels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('olevels');
+    }
+}
